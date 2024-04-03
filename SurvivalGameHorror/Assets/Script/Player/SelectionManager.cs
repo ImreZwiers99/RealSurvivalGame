e53cd6cf26bb2dcacb3aba0ee18da7402f54b9ec
ImreZwier99 <ifrzwiers@st.noorderpoort.nl>
2024-04-03 16:07:31 +0200

    as

---
+++ SurvivalGameHorror/Assets/Script/Player/SelectionManager.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SelectionManager : MonoBehaviour
+{
+    public GameObject interaction_Info_UI;
+    TextMeshProUGUI interaction_text;
+    public static bool inRange = false;
+    //public static bool isHit;
+
+    private void Start()
+    {
+        interaction_text = interaction_Info_UI.GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool isHit = Physics.Raycast(ray, out hit);
+        if (isHit)
+        {
+            var selectionTransform = hit.transform;
+
+            if (selectionTransform.GetComponent<InteractableObject>() && selectionTransform.GetComponent<InteractableObject>().playerInRange)
+            {
+                //inRange = true;
+                interaction_text.text = selectionTransform.GetComponent<InteractableObject>().GetItemName();
+                interaction_Info_UI.SetActive(true);
+            }
+            else
+            {
+                interaction_Info_UI.SetActive(false);
+            }
+        }
+        else
+        {
+            //inRange = false;
+            interaction_Info_UI.SetActive(false);
+        }
+    }
+}
